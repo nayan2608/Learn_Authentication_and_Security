af5058c48b718177b985f758b76713566d830760
nayan2608 <nayanramani26@gmail.com>
2022-10-01 12:03:15 +0530

    level-3 use hash function md5

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,9 +24,6 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
-
 const User = mongoose.model('User', userSchema);
 
 app.get("/", function(req, res){
@@ -43,7 +41,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newuser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newuser.save(function(err){
@@ -58,7 +56,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, founduser){
         if (err){
