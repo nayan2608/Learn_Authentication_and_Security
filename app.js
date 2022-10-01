cf3833d6595db8febc31c5aabacb75ec190780a9
nayan2608 <nayanramani26@gmail.com>
2022-10-01 11:31:23 +0530

    Use environment variable to keep secrets safe

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
@@ -23,8 +23,8 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const secret = "Thisismylittlesecret";
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
 
 const User = mongoose.model('User', userSchema);
 
